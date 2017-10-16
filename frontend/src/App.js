eb648f7fc4f11d890ab7a839d6810dbb202175f5
Fernando Henrique Bandeira <fernando.bandeira94@gmail.com>
2017-10-16 10:54:07 -0200

    Start root page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import { Container } from 'semantic-ui-react'
+import { Route } from 'react-router-dom';
+import Root from './pages/Root';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        Teste
-      </div>
+      <Container style={{ marginTop: '40px' }}>
+        <Route path="/" exact component={Root}/>
+        <Route path="/c/:category" component={Root}/>
+      </Container>
     );
   }
 }
